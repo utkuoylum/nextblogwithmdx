e799b87da909b19aa5bc70a69e5635617be54253
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-07-11 17:13:48 +0200

    import fix

--- app/recipe/[slug]/page.js
+++ app/recipe/[slug]/page.js
@@ -5,7 +5,7 @@ import fs from "fs"
 import matter from "gray-matter"
 
 function getPostContent(slug) {
-    const folder = 'recipes/'
+    const folder = './recipes/'
     const file = folder + `${slug}.md`
     const content = fs.readFileSync(file, 'utf8')
 
